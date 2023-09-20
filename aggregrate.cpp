bd9210ed686c4a2c81abec24174e91e365a2e8e9
Hamdanaleem <144408426+Hamdanaleem@users.noreply.github.com>
2023-09-20 09:06:38 +0500

    Add files via upload

---
+++ aggregrate.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+using namespace std;
+main(){
+
+	cout<<"Enter your Name:";
+	string name;
+	cin>>name;
+	cout<<"Enter your Roll no:";
+	int rollno;
+	cin>>rollno;
+	cout<<"Enter your aggregrate: ";
+	double aggregrate;
+	cin>>aggregrate;
+	cout<<"Enter your section:";
+	char section;
+	cin>>section;
+	cout<<"Name          :"<<name<<endl;
+	cout<<"Roll no         :"<<rollno<<endl;
+	cout<<"Aggregrate  :"<<aggregrate<<endl;
+	cout<<"Section        :"<<section<<endl;  
+
+
+
+
+
+
+
+
+
+
+
+}
