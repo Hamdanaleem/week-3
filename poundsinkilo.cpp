a7665ea4238d7e26abb0b657161c04cee72b14f2
Hamdanaleem <144408426+Hamdanaleem@users.noreply.github.com>
2023-09-20 09:20:11 +0500

    Add files via upload

---
+++ poundsinkilo.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+using namespace std;
+main(){
+	cout<<"Enter Weight in Pounds ";
+	int pound;
+	cin>>pound;
+	double kilogram;                              
+	kilogram= pound*0.45;	
+	cout<<pound<<"pounds is equal to  "<<kilogram<<"   kilograms";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
