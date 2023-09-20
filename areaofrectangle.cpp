cb8d05f647710add1b91c8ef984185e8ffb03164
Hamdanaleem <144408426+Hamdanaleem@users.noreply.github.com>
2023-09-20 09:29:16 +0500

    Add files via upload

---
+++ areaofrectangle.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+main(){
+	cout<<"Enter the length of rectangle  :";
+	int length;
+	cin>>length;
+	cout<<"Enter the width of rectangle  :";
+	int width;
+	cin>>width;
+	int area;
+	area=length*width;
+	cout<<"Area of rectangle is  :  "<<area<<endl;
+
+
+
+
+
+
+
+
+
+
+
+}
