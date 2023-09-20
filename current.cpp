8c17ce2a93b912a76e631382d92eec3075202454
Hamdanaleem <144408426+Hamdanaleem@users.noreply.github.com>
2023-09-20 11:01:09 +0500

    Add files via upload

---
+++ current.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+main(){
+	cout<<"Enter the charge (Q) in Coulombs: ";
+	float charge;
+	cin>>charge;
+	cout<<"Enter the time (t) in seconds: ";
+	float time;
+	cin>>time;
+	double current;
+	current=charge/time;
+	cout<<"The current (I) is: "<<current<<" Amperes";
+
+
+
+
+
+
+
+
+
+
+}
